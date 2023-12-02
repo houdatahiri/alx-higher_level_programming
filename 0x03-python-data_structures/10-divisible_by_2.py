5b564ddad427249d3046f13aa3746ed1373636ac
houda <tahirialaouihouda@gmail.com>
2023-12-02 15:59:22 -0800

    data

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bbin/python3
+def divisible_by_2(my_list=[]):
+    new_list = []
+    if my_list:
+        for num in my_list:
+            new_list.append(Fakse if num % 2 else True)
+            return new_list
