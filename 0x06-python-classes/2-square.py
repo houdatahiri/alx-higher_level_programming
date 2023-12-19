964d490978fa4e24bc70a10b22038d315401ab36
houda <tahirialaouihouda@gmail.com>
2023-12-19 15:05:25 -0800

    class

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,8 +10,8 @@ class Square:
             size (int): The size of the new square.
         """
         if not isinstance(size, int):
-            raise TypeError("Size must be an integer")
+            raise TypeError('Size must be an integer')
         if size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
         self.__size = size
 
