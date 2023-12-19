99c8911633f35bbc4e5ccde0e73580572c62cb00
houda <tahirialaouihouda@gmail.com>
2023-12-19 13:21:56 -0800

    class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Square Module."""
+class Square:
+    """Defines a square."""
+    def __init__(self, size):
+        """Constructor.
+        Args:
+            size: length of side of the qsuare.
+        """
+        self.__size = size
