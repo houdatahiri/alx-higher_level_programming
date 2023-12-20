3b3358695cb7c7ccac675340cda25a904ae94076
houda <tahirialaouihouda@gmail.com>
2023-12-20 08:27:02 -0800

    class

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """Define a class Square."""
 
+
 class Square:
     """Represent a square."""
-    
     def __init__(self, size=0):
         """Initialize a new square.
-        
         Args:
             Size (int): The size of the new square.
         """
