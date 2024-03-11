726de7fc6e49afbf485ad9e5d56fc1d7cbecac97
houda <tahirialaouihouda@gmail.com>
2024-03-11 22:17:52 +0000

    js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,5 +4,5 @@ if (process.argv.length <= 3) {
 } else {
   const arr = process.argv.slice(2).map(Number);
   const second = arr.sort(function (a, b) { return b - a; }) [1];
-  consolee.log(second);
+  console.log(second);
 }
