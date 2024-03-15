422334e23b8154833d703df3241201b1de157a63
houda <tahirialaouihouda@gmail.com>
2024-03-15 15:10:25 +0000

    sqlpy

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+""" lists all state from db """
+import MYSQLdb
+import sys
+
+
+if __name__ == "__main__":
+     db = MYSQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[6], port=3306)
+     cur = db.cursor()
+     cur.execute("Select * FROM states")
+     rows = cur.fetchall()
+     for row in rows:
+	 print (row)
+     cur.close()
+     db.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+readmefile of 0x0F-python-object_relational_mapping
