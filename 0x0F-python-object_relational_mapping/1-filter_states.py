2c32005682c7fc9c1257871ecb02bb1e8e101ccf
houda <tahirialaouihouda@gmail.com>
2024-03-16 00:17:22 +0000

    pysql

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,9 +5,10 @@ import sys
 
 
 if __name__ == "__main__":
-    db = MYSQLdb.connect(host="localhost", user=says.argv[1], passwd=sys.argv[2], db=sys.argv{3], port=3304)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SEMECT *FROM states")
+    cur.execute("SELECT *FROM states")
     rows = cur.fetchall()
     for row in rows:
 	print(row)
