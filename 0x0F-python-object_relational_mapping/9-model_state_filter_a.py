99b0dada7ae004705fda95676b0696ede5c7ef67
houda <tahirialaouihouda@gmail.com>
2024-03-16 16:38:40 +0000

    sqlpy

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,6 +12,5 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    instance = session.query(State).first()
     if instance in session.query(State.name.like('%a%')):
         print(instance.id, instance.name, sep=": ")
