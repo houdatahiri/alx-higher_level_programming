9916fa4e234a93544fec59deb2c98a42b3efdb2b
houda <tahirialaouihouda@gmail.com>
2024-03-16 16:42:06 +0000

    sqlpy

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -12,5 +12,8 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State.name.like('%a%')):
-        print(instance.id, instance.name, sep=": ")
+    instance = session.query(State).filter(State.name == (sys.argv[4],))
+    try:
+        print(instance[0].id)
+    except IndexError:
+        print("Not found")
