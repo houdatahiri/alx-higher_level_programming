dcd3ac62d45fd570fd238a93c61b83a425afec93
houda <tahirialaouihouda@gmail.com>
2024-03-16 16:14:28 +0000

    sqlpy

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,4 +1,6 @@
-ontains State class and Base, an instance of declarative_base()
+#!/usr/bin/python3
+"""
+Contains State class and Base, an instance of declarative_base()
 """
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
