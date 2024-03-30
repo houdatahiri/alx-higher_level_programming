7c9c340179e7464a5b4f00e82295cb16fa8e83a3
houda <tahirialaouihouda@gmail.com>
2024-03-30 07:24:03 -0700

    network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -sI "%1" | grep -i content-length
+rep=$(curl -sI "%1" | grep -i content-length | awk '/content-length:/ {print $2}')
+echo "'$1': $rep"
