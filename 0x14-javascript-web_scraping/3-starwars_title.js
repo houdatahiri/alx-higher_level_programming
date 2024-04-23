7a52d01c658e67826e2b9179a5f9ca37b263f8b5
houda <tahirialaouihouda@gmail.com>
2024-04-23 12:26:57 -0700

    js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-//Import the 'request' module.
+// Import the 'request' module.
 const request = require('request');
-// Construct the URL for the specific Star Wars fil
-const url = 'https://swapi-api.alx-tools.comm/api/films/' + process.argv[2];
-// Use the 'request' module to preform an HTTP GET request to the constructed URL.
-request(url, function(error, response, body){
-  // log title is successful, log error if not.
+// Construct the URL for the specific Star Wars film
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+// Use the 'request' module to perform an HTTP GET request to the constructed URL.
+request(url, function (error, response, body) {
+  // log title if successful, log error if not.
   console.log(error || JSON.parse(body).title);
 });
